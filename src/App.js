8b0696aec26da7cd994cb0fe8bff49124903a717
TadasL0 <119298439+TadasL0@users.noreply.github.com>
2023-01-22 20:12:30 +0200

    Revamp website altogether

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Navbar from './Navbar'; //import the navbar component
+import SideNavbar from './ui_navbar.js';
 import './index.css';
 
 function TextArea() {
   const [textboxes, setTextboxes] = useState([{ date: new Date().toDateString(), content: "" }]);
-  const [currentTextbox, setCurrentTextbox] = useState(0);
   const [inputText, setInputText] = useState("");
   const [showChatbot, setShowChatbot] = useState(false);
 
@@ -14,7 +13,7 @@ function TextArea() {
 
   return (
     <div>
-    <Navbar /> //render the navbar component
+    <SideNavbar /> 
     <div className="textareas-container">
       <textarea
         id="main-textarea"
@@ -23,12 +22,11 @@ function TextArea() {
         onChange={event => {
           setInputText(event.target.value);
           const newTextboxes = [...textboxes];
-          newTextboxes[currentTextbox].content = event.target.value;
           setTextboxes(newTextboxes);
         }}
       ></textarea>
       <div className={`chatbot-icon-container ${showChatbot ? 'show' : 'hide'}`} onClick={handleChatbotClick}>
-        <img src="https://ibb.co/1v7bb7n" alt="Chatbot Icon" className="chatbot-icon" />
+        <img src="src\Bobber.png" alt="Chatbot Icon" className="chatbot-icon" />
       </div>
     </div>
     </div>
---
+++ src/ui_navbar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './ui_navbar.css';
+
+const SideNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="side-navbar">
+      <div className="navbar-header">
+        <div className="hamburger-icon" onClick={() => setIsOpen(!isOpen)}>
+          &#9776;
+        </div>
+        <div className="plus-icon" onClick={() => alert("Add new diary entry")}>
+          +
+        </div>
+      </div>
+      <div className={`navbar-container ${isOpen ? 'open' : 'closed'}`}>
+        <div className="navbar-item">Diary Entry</div>
+        <div className="navbar-item">Diary Entry</div>
+        <div className="navbar-item">Diary Entry</div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNavbar;
